e4b0ab0ac0a4df7633658119826a0845b58d135a
shahryar-karimi <shahryarkarimi20@gmail.com>
2025-01-22 18:48:59 +0330

    feat: urls added

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('predict/', PredictDiceAPI.as_view(), name='predict'),
     path('count-down/', CountDownResultAPI.as_view(), name='countDown'),
     path('end-event/', EndCountDownResultAPI.as_view(), name='end-event'),
-
+    path('winners/', LastWinnersAPI.as_view(), name='winners'),
+    path('connect-wallet/', ConnectWalletAPI.as_view(), name='connect-wallet'),
+    path('player/', PlayerInfoAPI.as_view(), name='end-event'),
 
 ]
