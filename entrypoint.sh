53dd4e360cde03c2439c85c2a8a810aaa51db9ab
Behrad <behrad.fathi@igikala.com>
2025-01-25 14:14:42 +0330

    feat: update docker configs

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ set -e
 # then the rest of the code run as well.
 #env > .env
 
-#echo "Waiting for database..."
-#echo DB_NAME: ${DB_NAME}
-#echo DB_HOST: ${DB_HOST}
-#echo DB_PORT: ${DB_PORT}
-#while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 1; done
-#echo "Connected to database."
+echo "Waiting for database..."
+echo DB_NAME: ${DB_NAME}
+echo DB_HOST: ${DB_HOST}
+echo DB_PORT: ${DB_PORT}
+while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 1; done
+echo "Connected to database."
 
 if [ ${MIGRATE} == 'True' ]; then
     python manage.py migrate --no-input
