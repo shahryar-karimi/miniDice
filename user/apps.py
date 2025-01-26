76c289d5cb2d2e59e87be3a6dbd53abd2fe4ebb0
shahryar-karimi <shahryarkarimi20@gmail.com>
2025-01-26 19:01:18 +0330

    fix: dockerfile mahmoud7

--- user/apps.py
+++ user/apps.py
@@ -1,14 +1,13 @@
 from django.apps import AppConfig
-from django.core.management import call_command
 
 
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
 
-    def ready(self):
-        # Ensure superuser creation on startup
-        try:
-            call_command('createsuperuser_if_none_exists')
-        except Exception as e:
-            pass
+    # def ready(self):
+    #     # Ensure superuser creation on startup
+    #     try:
+    #         call_command('createsuperuser_if_none_exists')
+    #     except Exception as e:
+    #         pass
