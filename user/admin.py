29a4ebf12c7ba892e1f3bbf2dfc8370a0d06e316
shahryar-karimi <shahryarkarimi20@gmail.com>
2025-02-02 11:11:58 +0330

    fix: admin connected wallet filter

--- user/admin.py
+++ user/admin.py
@@ -1,14 +1,33 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from user.models import Player, Prediction, CountDown
 
 
 # Register your models here.
+class NullExistFilter(admin.SimpleListFilter):
+    title = _('wallet connected')  # Display name in the filter section
+    parameter_name = 'wallet_address'  # URL query parameter
+
+    def lookups(self, request, model_admin):
+        return [
+            ('none', _('Not Connected')),
+            ('exist', _('Connected Wallet'))
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'none':
+            return queryset.filter(wallet_address__isnull=True)
+        if self.value() == 'exist':
+            return queryset.filter(wallet_address__isnull=False)
+        return queryset  # Default: show all
+
+
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ("telegram_id", "telegram_username", "telegram_language_code", "auth_token", "wallet_address")
     search_fields = ("telegram_id", "telegram_username")
-    list_filter = ("is_active",)
+    list_filter = ("is_active", NullExistFilter)
     fieldsets = (
         (None,
          {'fields': ("telegram_id", "telegram_username", "telegram_language_code", "auth_token", "wallet_address")},),
@@ -18,7 +37,8 @@ class PlayerAdmin(admin.ModelAdmin):
 
 @admin.register(Prediction)
 class PredictionAdmin(admin.ModelAdmin):
-    list_display = ("player", "insert_dt", "countdown", "dice_number1", "dice_number2", "is_win", "is_active", "wallet", "amount")
+    list_display = (
+        "player", "insert_dt", "countdown", "dice_number1", "dice_number2", "is_win", "is_active", "wallet", "amount")
     search_fields = ("player__telegram_username", "player__telegram_id")
     list_filter = ("dice_number1", "dice_number2", "is_win", "is_active", "countdown")
     fieldsets = (
@@ -42,7 +62,8 @@ class PredictionAdmin(admin.ModelAdmin):
 
 @admin.register(CountDown)
 class CountDownAdmin(admin.ModelAdmin):
-    list_display = ("insert_dt", "expire_dt", "dice_number1", "dice_number2", "is_active", "amount", "won_players", "is_finished")
+    list_display = (
+        "insert_dt", "expire_dt", "dice_number1", "dice_number2", "is_active", "amount", "won_players", "is_finished")
     list_filter = ("is_active",)
     fieldsets = (
         (None,
