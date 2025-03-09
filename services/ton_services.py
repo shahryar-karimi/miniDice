3cf438fb079263ebe04044438ce365b44c918971
Alireza <alirezasakhaeirad@gmail.com>
2025-03-09 18:54:38 +0330

    new reqs

--- services/ton_services.py
+++ services/ton_services.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from tqdm import tqdm
 
 def _get_jetton_price(jetton_address):
     options = webdriver.ChromeOptions()
