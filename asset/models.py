c089fa2ece07128711de63f4f1b19fe3e85b61b9
Alireza <alirezasakhaeirad@gmail.com>
2025-03-10 18:53:21 +0330

    new admin sync asset and asset model added master address

--- asset/models.py
+++ asset/models.py
@@ -6,6 +6,7 @@ from user.models import Player
 
 class Asset(AbstractModel):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    master_address = models.CharField(max_length=255, null=True, blank=True)
     symbol = models.CharField(max_length=255, null=True, blank=True)
     name = models.CharField(max_length=255, null=True, blank=True)
     balance = models.IntegerField(max_digits=32, decimal_places=2, null=True, blank=True)
