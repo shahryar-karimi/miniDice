264e07f3334730192e9632d1ab06e3cc47b30d32
Alireza <alirezasakhaeirad@gmail.com>
2025-03-10 18:49:25 +0330

    deleted token model and updated Asset model

--- asset/models.py
+++ asset/models.py
@@ -7,24 +7,14 @@ from user.models import Player
 class Asset(AbstractModel):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     symbol = models.CharField(max_length=255, null=True, blank=True)
-    balance = models.DecimalField(max_digits=32, decimal_places=2, null=True, blank=True)
-    decimal = models.PositiveIntegerField(null=True, blank=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    balance = models.IntegerField(max_digits=32, decimal_places=2, null=True, blank=True)
+    decimal = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=56, decimal_places=24, null=True, blank=True)
+    usd_value = models.DecimalField(max_digits=56, decimal_places=24, null=True, blank=True)
 
     class Meta:
         db_table = 'assets'
         verbose_name = 'Asset'
         verbose_name_plural = 'Assets'
 
-#
-# class Token(AbstractModel):
-#     token_symbol = models.CharField(max_length=255)
-#     token_friendly_address = models.CharField(max_length=255, null=True)
-#     price = models.DecimalField(max_digits=32, decimal_places=24, null=True)
-#
-#     class Meta:
-#         db_table = 'tokens'
-#         verbose_name = 'Token'
-#         verbose_name_plural = 'Tokens'
-#
-#
-#
